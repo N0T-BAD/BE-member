64cb1530b44bdddc36643de7c33a477f14d88035
tvfxq0214@gmail.com <tvfxq0214@gmail.com>
2023-05-03 20:34:34 +0900

    feat: 멤버엔티티, 멤버 레파지토리 생성

---
+++ member-service/src/main/java/com/blockpage/memberservice/adaptor/infrastructure/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.blockpage.memberservice.adaptor.infrastructure;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+    Optional<MemberEntity> findByEmail(String email);
+}
