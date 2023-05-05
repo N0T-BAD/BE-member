cda8c16bed201d2f39d2aeb662b10093922e6544
tvfxq0214@gmail.com <tvfxq0214@gmail.com>
2023-05-06 03:00:58 +0900

    feat: 평점 리퀘스트 생성

---
+++ member-service/src/main/java/com/blockpage/memberservice/application/port/in/RequestRating.java
@@ -0,0 +1,11 @@
+package com.blockpage.memberservice.application.port.in;
+
+import lombok.Getter;
+
+@Getter
+public class RequestRating {
+
+    private Long episodeId;
+
+    private Integer ratings;
+}
