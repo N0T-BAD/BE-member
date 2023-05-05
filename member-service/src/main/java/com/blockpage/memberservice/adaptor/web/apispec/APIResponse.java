9d3a1887a83773f7b70ed014db7e6a9a078b93c9
tvfxq0214@gmail.com <tvfxq0214@gmail.com>
2023-05-05 22:09:28 +0900

    feat: API 뷰모델 생성

---
+++ member-service/src/main/java/com/blockpage/memberservice/adaptor/web/apispec/APIResponse.java
@@ -0,0 +1,18 @@
+package com.blockpage.memberservice.adaptor.web.apispec;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class APIResponse<T> {
+
+    private final T data;
+
+
+    public APIResponse(T data) {
+        this.data = data;
+    }
+}
