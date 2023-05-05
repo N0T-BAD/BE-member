284ad9ed4150a2816ac1607470592c0b9fc0ed00
tvfxq0214@gmail.com <tvfxq0214@gmail.com>
2023-05-05 23:14:14 +0900

    refactor: 멤버 뷰모델 생성자 추가

---
+++ member-service/src/main/java/com/blockpage/memberservice/adaptor/web/view/MemberView.java
@@ -0,0 +1,85 @@
+package com.blockpage.memberservice.adaptor.web.view;
+
+import com.blockpage.memberservice.adaptor.infrastructure.value.Role;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class MemberView {
+
+    private Long id;
+
+    //MemberView
+    private String nickname;
+
+    private String profileImage;
+
+    private String profileSkin;
+
+    private Role role;
+
+    private String creatorNickname;
+
+    //InterestView
+    private Long webtoonId;
+
+    private String webtoonTitle;
+
+    private String webtoonThumbnail;
+
+    private String creator;
+
+    private String illustrator;
+
+    private String genre;
+
+    //EmotionView
+    private Long commentId;
+
+    private Boolean emotion;
+
+    //RatingView
+    private Integer ratings;
+
+    //회원조회
+    public MemberView(String nickname, String profileImage, String profileSkin, Role role, String creatorNickname) {
+        this.nickname = nickname;
+        this.profileImage = profileImage;
+        this.profileSkin = profileSkin;
+        this.role = role;
+        this.creatorNickname = creatorNickname;
+    }
+
+    //찜목록 조회
+    public MemberView(Long id, Long webtoonId, String webtoonTitle, String webtoonThumbnail, String creator, String illustrator,
+        String genre) {
+        this.id = id;
+        this.webtoonId = webtoonId;
+        this.webtoonTitle = webtoonTitle;
+        this.webtoonThumbnail = webtoonThumbnail;
+        this.creator = creator;
+        this.illustrator = illustrator;
+        this.genre = genre;
+    }
+
+    //댓글반응 조회
+    public MemberView(Long id, Long commentId, Boolean emotion) {
+        this.id = id;
+        this.commentId = commentId;
+        this.emotion = emotion;
+    }
+
+    //평점조회
+    public MemberView(Integer ratings) {
+        this.ratings = ratings;
+    }
+}
