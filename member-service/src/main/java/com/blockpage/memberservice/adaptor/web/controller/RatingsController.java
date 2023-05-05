f69bf14c485ab5aecf9405975d5b061e603feb0c
tvfxq0214@gmail.com <tvfxq0214@gmail.com>
2023-05-06 03:08:51 +0900

    feat: 평점 추가 Mock API 생성

--- member-service/src/main/java/com/blockpage/memberservice/adaptor/web/controller/RatingsController.java
+++ member-service/src/main/java/com/blockpage/memberservice/adaptor/web/controller/RatingsController.java
@@ -1,11 +1,17 @@
 package com.blockpage.memberservice.adaptor.web.controller;
 
+import com.blockpage.memberservice.adaptor.infrastructure.MemberEntity;
+import com.blockpage.memberservice.adaptor.infrastructure.view.Role;
+import com.blockpage.memberservice.adaptor.infrastructure.entity.RatingEntity;
 import com.blockpage.memberservice.adaptor.web.apispec.APIResponse;
 import com.blockpage.memberservice.adaptor.web.view.MemberView;
+import com.blockpage.memberservice.application.port.in.RequestRating;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +21,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/ratings")
 @Slf4j
 public class RatingsController {
-
-    @GetMapping
-    public ResponseEntity<APIResponse> getInterests(@RequestParam("episodeId") Long episodeId) {
-        return ResponseEntity.ok().body(new APIResponse(new MemberView(8)));
+    @PostMapping
+    public ResponseEntity<APIResponse> addRating(@RequestBody RequestRating requestRating) {
+        RatingEntity ratingEntity = RatingEntity.builder()
+            .memberEntity(
+                MemberEntity.builder().
+                    email("abc@naver.com")
+                    .nickname("고은")
+                    .profileImage("프로필이미지")
+                    .role(Role.MEMBER)
+                    .adult(false)
+                    .build())
+            .episodeId(1L)
+            .ratings(8)
+            .build();
+        return ResponseEntity.ok().body(new APIResponse("평점 등록되었습니다.", requestRating.getRatings()));
     }
 
+    @GetMapping
+    public ResponseEntity<APIResponse> getInterest(@RequestParam("episodeId") Long episodeId) {
+        return ResponseEntity.ok().body(new APIResponse(new MemberView(8)));
+    }
 
 }
