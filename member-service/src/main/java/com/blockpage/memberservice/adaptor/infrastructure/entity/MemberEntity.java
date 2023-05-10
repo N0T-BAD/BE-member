8b60ca4f08c7fcb7e0b144a901b04bb29af94e09
tvfxq0214@gmail.com <tvfxq0214@gmail.com>
2023-05-10 10:06:44 +0900

    refactor: 스프링 버전 변경에 따른 엔티티 수정 및 파일 변경

--- member-service/src/main/java/com/blockpage/memberservice/adaptor/infrastructure/entity/MemberEntity.java
+++ member-service/src/main/java/com/blockpage/memberservice/adaptor/infrastructure/entity/MemberEntity.java
@@ -1,7 +1,13 @@
 package com.blockpage.memberservice.adaptor.infrastructure.entity;
 
 import com.blockpage.memberservice.adaptor.infrastructure.view.Role;
-import jakarta.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,6 +23,9 @@ public class MemberEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
+    private Long kakaoId;
 
     @Column
     private String email;
--- member-service/src/main/java/com/blockpage/memberservice/adaptor/infrastructure/repository/InterestRepository.java
+++ member-service/src/main/java/com/blockpage/memberservice/adaptor/infrastructure/repository/InterestRepository.java
@@ -4,6 +4,7 @@ import com.blockpage.memberservice.adaptor.infrastructure.entity.InterestEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-public interface InterestRepository extends JpaRepository<InterestEntity,Long> {
+public interface InterestRepository extends JpaRepository<InterestEntity, Long> {
+
     List<InterestEntity> findAllByMemberEntityId(Long memberId);
 }
