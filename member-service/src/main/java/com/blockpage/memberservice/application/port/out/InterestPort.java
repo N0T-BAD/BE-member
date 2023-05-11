f8a3d936b14dedb0d29b52ab0c8640820e4166d2
tvfxq0214@gmail.com <tvfxq0214@gmail.com>
2023-05-12 08:11:06 +0900

    feat: 찜 Adaptor와 Port 생성

---
+++ member-service/src/main/java/com/blockpage/memberservice/application/port/out/InterestPort.java
@@ -0,0 +1,8 @@
+package com.blockpage.memberservice.application.port.out;
+
+import com.blockpage.memberservice.domain.Interest;
+
+public interface InterestPort {
+
+    void saveInterest(Interest interest);
+}
