35c8c14ad1ba5aa9d89a138a09719361fbc432dc
tvfxq0214@gmail.com <tvfxq0214@gmail.com>
2023-05-12 10:48:53 +0900

    feat: 찜 조회 유즈케이스와 서비스 기능 추가

--- member-service/src/main/java/com/blockpage/memberservice/application/port/in/InterestUseCase.java
+++ member-service/src/main/java/com/blockpage/memberservice/application/port/in/InterestUseCase.java
@@ -2,12 +2,15 @@ package com.blockpage.memberservice.application.port.in;
 
 import com.blockpage.memberservice.adaptor.infrastructure.entity.MemberEntity;
 import com.blockpage.memberservice.application.port.out.InterestDto;
+import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 
 public interface InterestUseCase {
 
     InterestDto saveInterestQuery(SaveQuery query, MemberEntity memberEntity);
+
+    List<InterestDto> findInterestQuery(FindQuery query);
 
     @Getter
     @Builder
@@ -28,6 +31,20 @@ public interface InterestUseCase {
                 .creator(requestInterest.getCreator())
                 .illustrator(requestInterest.getIllustrator())
                 .genre(requestInterest.getGenre())
+                .build();
+        }
+    }
+
+    @Getter
+    @Builder
+    class FindQuery {
+
+        private Long memberId;
+
+        public static FindQuery toQuery(Long id) {
+
+            return FindQuery.builder()
+                .memberId(id)
                 .build();
         }
     }
