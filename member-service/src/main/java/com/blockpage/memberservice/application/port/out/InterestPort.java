e8d4b2d4feaa7750db24e43fa74d2369781979b3
tvfxq0214@gmail.com <tvfxq0214@gmail.com>
2023-05-12 10:53:02 +0900

    feat: 찜 조회 포트와 업뎁터 기능 추가

--- member-service/src/main/java/com/blockpage/memberservice/application/port/out/InterestPort.java
+++ member-service/src/main/java/com/blockpage/memberservice/application/port/out/InterestPort.java
@@ -1,8 +1,11 @@
 package com.blockpage.memberservice.application.port.out;
 
 import com.blockpage.memberservice.domain.Interest;
+import java.util.List;
 
 public interface InterestPort {
 
     void saveInterest(Interest interest);
+
+    List<Interest> findInterest(Long memberId);
 }
