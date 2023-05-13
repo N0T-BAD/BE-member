50ed7cc023b7d367883258e9686c58f87fb3903d
tvfxq0214@gmail.com <tvfxq0214@gmail.com>
2023-05-14 02:25:24 +0900

    feat: 댓글반응 API 생성 및 댓글반응DTO 뷰 변환 생성

--- member-service/src/main/java/com/blockpage/memberservice/adaptor/web/controller/EmotionController.java
+++ member-service/src/main/java/com/blockpage/memberservice/adaptor/web/controller/EmotionController.java
@@ -1,14 +1,18 @@
 package com.blockpage.memberservice.adaptor.web.controller;
 
-import com.blockpage.memberservice.adaptor.infrastructure.entity.EmotionEntity;
 import com.blockpage.memberservice.adaptor.infrastructure.entity.MemberEntity;
-import com.blockpage.memberservice.adaptor.infrastructure.view.Role;
 import com.blockpage.memberservice.adaptor.web.view.ApiResponse;
 import com.blockpage.memberservice.adaptor.web.view.MemberView;
+import com.blockpage.memberservice.application.port.in.EmotionUseCase;
+import com.blockpage.memberservice.application.port.in.EmotionUseCase.DeleteQuery;
+import com.blockpage.memberservice.application.port.in.EmotionUseCase.FindQuery;
+import com.blockpage.memberservice.application.port.in.EmotionUseCase.SaveQuery;
 import com.blockpage.memberservice.application.port.in.RequestEmotion;
-import java.util.ArrayList;
+import com.blockpage.memberservice.application.port.out.EmotionDto;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,8 +20,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,37 +29,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/emotions")
 public class EmotionController {
 
+    private final EmotionUseCase emotionUseCase;
+
     @PostMapping
-    private ResponseEntity<ApiResponse> addEmotion(@RequestBody RequestEmotion requestEmotion) {
-        EmotionEntity emotion = EmotionEntity.builder()
-            .memberEntity(
-                MemberEntity.builder().
-                    email("abc@naver.com")
-                    .nickname("고은")
-                    .profileImage("프로필이미지")
-                    .role(Role.MEMBER)
-                    .adult(false)
-                    .build())
-            .episodeId(requestEmotion.getEpisodeId())
-            .commentId(requestEmotion.getEpisodeId())
-            .emotion(requestEmotion.getEmotion())
-            .build();
-        return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse(requestEmotion.getEmotion()));
+    public ResponseEntity<ApiResponse<String>> addEmotion(@RequestHeader("accessToken") String token,
+        @RequestBody RequestEmotion requestEmotion) {
+        MemberEntity memberEntity = MemberEntity.builder().id(1L).build();
+        SaveQuery saveQuery = SaveQuery.toQuery(requestEmotion);
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(new ApiResponse<String>(emotionUseCase.saveEmotionQuery(saveQuery, memberEntity)));
     }
 
     @GetMapping
-    public ResponseEntity<ApiResponse> getEmotion(@RequestParam("episodeId") Long episodeId) {
-        List<MemberView> memberViewList = new ArrayList<>();
-
-        memberViewList.add(new MemberView(1L, 1L, (Boolean) true));
-        memberViewList.add(new MemberView(2L, 3L, (Boolean) false));
-        memberViewList.add(new MemberView(3L, 5L, (Boolean) true));
-
-        return ResponseEntity.ok().body(new ApiResponse<>(memberViewList));
+    public ResponseEntity<ApiResponse<List<MemberView>>> getEmotion(@RequestHeader("accessToken") String token,
+        @Param("episodeId") Long episodeId) {
+        MemberEntity memberEntity = MemberEntity.builder().id(1L).build();
+        List<EmotionDto> emotionDtoList = emotionUseCase.findAllEmotionQuery(new FindQuery(episodeId), memberEntity);
+        List<MemberView> memberViewList = emotionDtoList.stream()
+            .map(emotionDto -> new MemberView(emotionDto))
+            .collect(Collectors.toList());
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>(memberViewList));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<ApiResponse> deleteEmotion(@PathVariable Long id) {
-        return ResponseEntity.ok().body(new ApiResponse("댓글 반응이 삭제 되엇습니다."));
+    public ResponseEntity<ApiResponse<String>> deleteEmotion(@PathVariable Long id) {
+        emotionUseCase.deleteEmotionQuery(new DeleteQuery(id));
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>("댓글반응이 삭제되었습니다."));
     }
+
 }
