0cfc97cffe136b9fc92053723c9a155c111f6502
tvfxq0214@gmail.com <tvfxq0214@gmail.com>
2023-05-13 17:04:46 +0900

    feat: 평점 등록 API 생성 & Mock API 삭제

--- member-service/src/main/java/com/blockpage/memberservice/adaptor/web/controller/RatingsController.java
+++ member-service/src/main/java/com/blockpage/memberservice/adaptor/web/controller/RatingsController.java
@@ -1,17 +1,19 @@
 package com.blockpage.memberservice.adaptor.web.controller;
 
 import com.blockpage.memberservice.adaptor.infrastructure.entity.MemberEntity;
-import com.blockpage.memberservice.adaptor.infrastructure.entity.RatingEntity;
-import com.blockpage.memberservice.adaptor.infrastructure.view.Role;
 import com.blockpage.memberservice.adaptor.web.view.ApiResponse;
 import com.blockpage.memberservice.adaptor.web.view.MemberView;
+import com.blockpage.memberservice.application.port.in.RatingUseCase;
+import com.blockpage.memberservice.application.port.in.RatingUseCase.SaveQuery;
 import com.blockpage.memberservice.application.port.in.RequestRating;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,21 +24,15 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class RatingsController {
 
+    private final RatingUseCase ratingUseCase;
+
     @PostMapping
-    public ResponseEntity<ApiResponse> addRating(@RequestBody RequestRating requestRating) {
-        RatingEntity ratingEntity = RatingEntity.builder()
-            .memberEntity(
-                MemberEntity.builder().
-                    email("abc@naver.com")
-                    .nickname("고은")
-                    .profileImage("프로필이미지")
-                    .role(Role.MEMBER)
-                    .adult(false)
-                    .build())
-            .episodeId(1L)
-            .ratings(8)
-            .build();
-        return ResponseEntity.ok().body(new ApiResponse(requestRating.getRatings()));
+    public ResponseEntity<ApiResponse<String>> addRating(@RequestBody RequestRating requestRating,
+        @RequestHeader("accessToken") String token) {
+        MemberEntity memberEntity = MemberEntity.builder().id(1L).build();
+        SaveQuery saveQuery = SaveQuery.toQuery(requestRating);
+        ratingUseCase.saveRatingQuery(saveQuery, memberEntity);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse<String>("평점 등록 되었습니다."));
     }
 
     @GetMapping
