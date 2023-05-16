5979e8dd7c36b9644073b4d66110860cb005eb9d
ssooyaa <tvfxq0214@gmail.com>
2023-05-16 18:46:37 +0900

    feat: 카카오로그인 리스폰스 바디 설정

---
+++ member-service/src/main/java/com/blockpage/memberservice/adaptor/external/kakao/ResponseBody/KakaoTokenResponse.java
@@ -0,0 +1,17 @@
+package com.blockpage.memberservice.adaptor.external.kakao.ResponseBody;
+
+import lombok.Getter;
+
+@Getter
+public class KakaoTokenResponse {
+
+    private String token_type;
+
+    private String access_token;
+
+    private String expires_in;
+
+    private String refresh_token;
+
+    private String refresh_token_expires_in;
+}
