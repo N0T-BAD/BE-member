5546a4d8e64dee58347120a66ae9412753e499fe
ssooyaa <tvfxq0214@gmail.com>
2023-05-18 18:31:53 +0900

    feat: 회원정보변경 및 조회 API 생성

--- member-service/src/main/java/com/blockpage/memberservice/adaptor/web/controller/MemberController.java
+++ member-service/src/main/java/com/blockpage/memberservice/adaptor/web/controller/MemberController.java
@@ -7,6 +7,7 @@ import com.blockpage.memberservice.application.port.in.MemberUseCase.FindMemberQ
 import com.blockpage.memberservice.application.port.in.MemberUseCase.UpdateQuery;
 import com.blockpage.memberservice.application.port.in.RequestMember;
 import com.blockpage.memberservice.application.port.out.MemberDto;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -16,7 +17,10 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 
 @RestController
@@ -28,95 +32,27 @@ public class MemberController {
     private final MemberUseCase memberUseCase;
 
     @PutMapping
-    public ResponseEntity<ApiResponse<MemberView>> updateMember(@RequestHeader("accessToken") String token,
-        @RequestBody RequestMember requestMember) {
-        memberUseCase.updateMemberInfo(UpdateQuery.toQuery(1L, requestMember));
+    public ResponseEntity<ApiResponse<MemberView>> updateMember(
+        @RequestHeader("accessToken") String token,
+        @RequestParam("type") String type,
+        @RequestPart RequestMember requestMember,
+        @RequestPart MultipartFile profileImage) throws IOException {
+        memberUseCase.updateMemberInfo(UpdateQuery.toQuery(1L, type, requestMember,profileImage));
         return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>(new MemberView("회원정보가 변경되었습니다.")));
     }
-//    @PutMapping
-//    public ResponseEntity<ApiResponse> updateMember(@RequestParam("type") String type, @RequestBody RequestMember requestMember) {
-//        switch (type) {
-//            case "nickname": {
-//                MemberEntity memberEntity = MemberEntity.builder()
-//                    .email("abc@naver.com")
-//                    .nickname(requestMember.getNickname())
-//                    .profileImage("https://user-images.githubusercontent.com/97498405/235885340-d63630ec-85ec-4801-bf73-ac83f96c3bd2.jpg")
-//                    .role(Role.MEMBER)
-//                    .adult(false)
-//                    .build();
-//                log.info(memberEntity.toString());
-//                return ResponseEntity.ok().body(new ApiResponse(requestMember.getNickname()));
-//            }
-//            case "profileImage": {
-//                MemberEntity memberEntity = MemberEntity.builder()
-//                    .email("abc@naver.com")
-//                    .nickname("고은")
-//                    .profileImage(requestMember.getProfileImage())
-//                    .role(Role.MEMBER)
-//                    .adult(false)
-//                    .build();
-//                log.info(memberEntity.toString());
-//                return ResponseEntity.ok().body(new ApiResponse(requestMember.getProfileImage()));
-//            }
-//            case "profileSkin": {
-//                MemberEntity memberEntity = MemberEntity.builder()
-//                    .email("abc@naver.com")
-//                    .nickname("고은")
-//                    .profileImage("https://user-images.githubusercontent.com/97498405/235885340-d63630ec-85ec-4801-bf73-ac83f96c3bd2.jpg")
-//                    .profileSkin(requestMember.getProfileSkin())
-//                    .role(Role.MEMBER)
-//                    .adult(false)
-//                    .build();
-//                log.info(memberEntity.toString());
-//                return ResponseEntity.ok().body(new ApiResponse(requestMember.getProfileSkin()));
-//            }
-//            case "author": {
-//                String addCreatorNickname = "인정";
-//                MemberEntity memberEntity = MemberEntity.builder()
-//                    .email("abc@naver.com")
-//                    .nickname("고은")
-//                    .profileImage("https://user-images.githubusercontent.com/97498405/235885340-d63630ec-85ec-4801-bf73-ac83f96c3bd2.jpg")
-//                    .role(Role.AUTHOR)
-//                    .creatorNickname(requestMember.getCreatorNickname())
-//                    .adult(false)
-//                    .build();
-//                log.info(memberEntity.toString());
-//                return ResponseEntity.ok().body(new ApiResponse(requestMember.getCreatorNickname()));
-//            }
-//            case "authorNickname": {
-//                MemberEntity memberEntity = MemberEntity.builder()
-//                    .email("abc@naver.com")
-//                    .nickname("고은")
-//                    .profileImage("https://user-images.githubusercontent.com/97498405/235885340-d63630ec-85ec-4801-bf73-ac83f96c3bd2.jpg")
-//                    .role(Role.AUTHOR)
-//                    .adult(false)
-//                    .creatorNickname(requestMember.getCreatorNickname())
-//                    .build();
-//                log.info(memberEntity.toString());
-//                return ResponseEntity.ok().body(new ApiResponse(requestMember.getCreatorNickname()));
-//            }
-//            case "adult": {
-//                MemberEntity memberEntity = MemberEntity.builder()
-//                    .email("abc@naver.com")
-//                    .nickname("고은")
-//                    .profileImage("https://user-images.githubusercontent.com/97498405/235885340-d63630ec-85ec-4801-bf73-ac83f96c3bd2.jpg")
-//                    .role(Role.MEMBER)
-//                    .adult(true)
-//                    .build();
-//                log.info(memberEntity.toString());
-//                return ResponseEntity.ok().body(new ApiResponse("성인으로 인증되었습니다."));
-//            }
-//            default: {
-//                return ResponseEntity.ok().body(new ApiResponse("회원정보 수정이 이루어지지 않았습니다."));
-//            }
-//        }
-//    }
 
-    @GetMapping
-    public ResponseEntity<ApiResponse<MemberView>> getMember(@RequestHeader("accessToken") String token) {
-        Long id = 1L;
-        FindMemberQuery findMemberQuery = new FindMemberQuery(id);
-        MemberDto memberDto = memberUseCase.findMemberinfo(findMemberQuery);
-        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>(new MemberView(memberDto)));
+    @GetMapping()
+    public ResponseEntity<ApiResponse<MemberView>> findMember(@RequestHeader(required = false, value = "accessToken") String token,
+        @RequestParam("type") String type,
+        @RequestBody(required = false) RequestMember requestMember) {
+        Long id = null;
+        MemberDto memberDto = memberUseCase.findMemberinfo(FindMemberQuery.toQuery(type, id, requestMember));
+        if (memberDto.getRole() != null) {
+            MemberView memberView = new MemberView(memberDto);
+            return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>(memberView));
+        } else {
+            MemberView memberView = new MemberView("사용가능한 닉네임입니다.");
+            return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>(memberView));
+        }
     }
 }
