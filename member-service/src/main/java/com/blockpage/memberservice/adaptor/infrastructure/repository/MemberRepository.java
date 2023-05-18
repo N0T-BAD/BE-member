98fd8f3aa73e0cf0eb7b2bcb698164ecf3d0a7b8
ssooyaa <tvfxq0214@gmail.com>
2023-05-18 18:28:40 +0900

    feat: 작가 닉네임 중복 조회 생성

--- member-service/src/main/java/com/blockpage/memberservice/adaptor/infrastructure/repository/MemberRepository.java
+++ member-service/src/main/java/com/blockpage/memberservice/adaptor/infrastructure/repository/MemberRepository.java
@@ -9,5 +9,6 @@ public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
 
     Optional<MemberEntity> findByKakaoId(Long kakaoId);
 
-    MemberEntity findByEmail(String email);
+    Optional<MemberEntity> findByCreatorNickname(String creatorNickname);
+
 }
