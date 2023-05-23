1a9ea165902dc2cb7b23afb3b4a0e45f9a86721d
ssooyaa <tvfxq0214@gmail.com>
2023-05-24 02:14:18 +0900

    refactor: 출석체크 session에서 email로 변경
    refactor: 출석체크 session에서 email로 변경

--- src/main/java/com/blockpage/memberservice/application/port/in/AttendanceUseCase.java
+++ src/main/java/com/blockpage/memberservice/application/port/in/AttendanceUseCase.java
@@ -1,6 +1,5 @@
 package com.blockpage.memberservice.application.port.in;
 
-import javax.servlet.http.HttpSession;
 import lombok.Getter;
 
 public interface AttendanceUseCase {
@@ -13,9 +12,7 @@ public interface AttendanceUseCase {
         String memberEmail;
 
 
-        public PostQuery(HttpSession session) {
-//            String email = (String) session.getAttribute("id");
-            String email = "test@naver.com";
+        public PostQuery(String email) {
             this.memberEmail = email;
         }
     }
