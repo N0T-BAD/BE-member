f880fdbb4c165b142d0ffadd01cb27e63350ed0c
ssooyaa <tvfxq0214@gmail.com>
2023-05-26 11:38:06 +0900

    refactor: 불필요 코드 삭제

--- src/main/java/com/blockpage/memberservice/application/port/out/EmotionDto.java
+++ src/main/java/com/blockpage/memberservice/application/port/out/EmotionDto.java
@@ -1,6 +1,5 @@
 package com.blockpage.memberservice.application.port.out;
 
-import com.blockpage.memberservice.adaptor.infrastructure.entity.EmotionEntity;
 import com.blockpage.memberservice.domain.Emotion;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -21,15 +20,6 @@ public class EmotionDto {
 
     public EmotionDto(Boolean emotion) {
         this.emotion = emotion;
-    }
-
-    public static EmotionDto fromEmotionEntity(EmotionEntity emotionEntity) {
-        return EmotionDto.builder()
-            .id(emotionEntity.getId())
-            .episodeId(emotionEntity.getEpisodeId())
-            .commentId(emotionEntity.getCommentId())
-            .emotion(emotionEntity.getEmotion())
-            .build();
     }
 
     public static EmotionDto fromEmotion(Emotion emotion) {
