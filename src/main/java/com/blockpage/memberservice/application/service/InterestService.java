39adc5517ead0a3648e7e300fc7d4c23205607f8
geunskoo <oryukdo3@pusan.ac.kr>
2023-05-29 15:49:29 +0900

    feat: 찜목록 전송을 위한 서비스로직 작성

--- src/main/java/com/blockpage/memberservice/application/service/InterestService.java
+++ src/main/java/com/blockpage/memberservice/application/service/InterestService.java
@@ -1,7 +1,9 @@
 package com.blockpage.memberservice.application.service;
 
+import com.blockpage.memberservice.adaptor.infrastructure.message.async.webtoon.message.InterestCountMessage;
 import com.blockpage.memberservice.application.port.in.InterestUseCase;
 import com.blockpage.memberservice.application.port.out.dto.InterestDto;
+import com.blockpage.memberservice.application.port.out.port.InterestMessagePort;
 import com.blockpage.memberservice.application.port.out.port.InterestPort;
 import com.blockpage.memberservice.domain.Interest;
 import java.util.List;
@@ -14,10 +16,14 @@ import org.springframework.stereotype.Service;
 public class InterestService implements InterestUseCase {
 
     private final InterestPort interestPort;
+    private final InterestMessagePort interestMessagePort;
+    private final Integer INTEREST_LIKE_COUNT = 1;
+    private final Integer INTEREST_DISLIKE_COUNT = -1;
 
     @Override
     public InterestDto postInterestQuery(PostQuery postQuery) {
         interestPort.postInterest(Interest.postInterest(postQuery));
+        interestMessagePort.sendInterestCount(InterestCountMessage.initMessage(postQuery.getWebtoonId(), INTEREST_LIKE_COUNT));
         return InterestDto.toQuery(postQuery);
     }
 
@@ -37,7 +43,7 @@ public class InterestService implements InterestUseCase {
 
     @Override
     public void deleteInterestQuery(DeleteQuery query) {
-        interestPort.deleteInterest(query.getId());
+        Interest interest = interestPort.deleteInterest(query.getId());
+        interestMessagePort.sendInterestCount(InterestCountMessage.initMessage(interest.getWebtoonId(), INTEREST_DISLIKE_COUNT));
     }
-
 }
