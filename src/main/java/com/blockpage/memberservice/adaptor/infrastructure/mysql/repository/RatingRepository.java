50dac8f582851d659611b18fa7fb7392b0d9dfe0
ssooyaa <tvfxq0214@gmail.com>
2023-05-30 01:13:16 +0900

    refactor: 평점 카프카 설정추가

--- src/main/java/com/blockpage/memberservice/adaptor/infrastructure/mysql/repository/RatingRepository.java
+++ src/main/java/com/blockpage/memberservice/adaptor/infrastructure/mysql/repository/RatingRepository.java
@@ -1,10 +1,14 @@
 package com.blockpage.memberservice.adaptor.infrastructure.mysql.repository;
 
 import com.blockpage.memberservice.adaptor.infrastructure.mysql.entity.RatingEntity;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RatingRepository extends JpaRepository<RatingEntity, Long> {
 
     Optional<RatingEntity> findByMemberEmailAndEpisodeId(String memberEmail, Long episodeId);
+
+    List<RatingEntity> findAllByRegisterTimeBetween(LocalDateTime start, LocalDateTime end);
 }
--- src/main/java/com/blockpage/memberservice/application/port/out/port/RatingPort.java
+++ src/main/java/com/blockpage/memberservice/application/port/out/port/RatingPort.java
@@ -1,10 +1,14 @@
 package com.blockpage.memberservice.application.port.out.port;
 
 import com.blockpage.memberservice.domain.Rating;
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface RatingPort {
 
     void postRating(Rating rating);
 
     Rating findRating(Rating rating);
+
+    List<Rating> findRatingByCreateDate(LocalDateTime start, LocalDateTime end);
 }
