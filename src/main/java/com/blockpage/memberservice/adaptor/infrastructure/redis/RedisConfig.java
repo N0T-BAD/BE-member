5e5cc4f419a98c6e95893981a11543922a257f17
ssooyaa <tvfxq0214@gmail.com>
2023-06-01 09:24:07 +0900

    feat: 레디스 세션 컨피그 작성

---
+++ src/main/java/com/blockpage/memberservice/adaptor/infrastructure/redis/RedisConfig.java
@@ -0,0 +1,51 @@
+package com.blockpage.memberservice.adaptor.infrastructure.redis;
+
+import java.time.Duration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
+import org.springframework.session.data.redis.RedisSessionRepository;
+import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
+
+@Configuration
+@EnableSpringHttpSession
+@RequiredArgsConstructor
+public class RedisConfig extends AbstractHttpSessionApplicationInitializer {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration(host, port);
+        return new LettuceConnectionFactory(redisConfig);
+    }
+
+    @Bean
+    public RedisOperations<String, Object> sessionRedisOperations() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        return template;
+    }
+
+    @Bean
+    public RedisSessionRepository sessionRepository(RedisOperations<String, Object> sessionRedisOperations) {
+        RedisSessionRepository redisSessionRepository = new RedisSessionRepository(sessionRedisOperations);
+        redisSessionRepository.setRedisKeyNamespace("spring");
+        redisSessionRepository.setDefaultMaxInactiveInterval(Duration.ofSeconds(60));
+        return redisSessionRepository;
+    }
+}
